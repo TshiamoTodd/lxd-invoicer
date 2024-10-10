955c9789b84de42522d70186d1f9a042d477a77b
TshiamoTodd <tshiamo.mokwena19@gmail.com>
2024-10-10 15:55:08 +0200

    Create dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,32 +8,79 @@ import {
     TableHeader,
     TableRow,
   } from "@/components/ui/table"
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { CirclePlus } from 'lucide-react'
+import Link from 'next/link'
   
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col items-center pt-10 justify-center h-full text-center gap-6 max-w-5xl mx-auto">
-        <h1 className="text-5xl font-bold">
-            Dashboard
+    <div className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto my-12">
+    <div className='flex justify-between gap-6'>
+        <h1 className="text-3xl font-bold">
+            Invoices
         </h1>
+        <p>
+            <Button variant={'ghost'} asChild className='hover:text-blue-500 flex gap-2'>
+                <Link href={'/invoices/new'}>
+                    <CirclePlus size={24} className='h-4 w-4' />
+                    Create Invoice
+                </Link>
+            </Button>
+        </p>
+    </div>
+
     <Table>
-    <TableCaption>A list of your recent invoices.</TableCaption>
-    <TableHeader>
-        <TableRow>
-            <TableHead className=" w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+            <TableRow>
+                <TableHead className=" w-[100px] p-4">
+                    Dates
+                </TableHead>
+                <TableHead className='p-4'>
+                    Customer
+                </TableHead>
+                <TableHead className='p-4'>
+                    Email
+                </TableHead>
+                <TableHead className='text-center p-4'>
+                    Status
+                </TableHead>
+                <TableHead className="text-right p-4">
+                    Value
+                </TableHead>
+                </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow>
+                <TableCell className="font-medium text-left p-4">
+                    <span className='font-semibold'>
+                        10/31/2024
+                    </span>
+                </TableCell>
+                <TableCell className='text-left p-4'>
+                    <span className='font-semibold'>
+                        Philip J Fry
+                    </span>
+                </TableCell>
+                <TableCell className='text-left p-4'>
+                    <span>
+                        fry@mailmonkey.com
+                    </span>
+                </TableCell>
+                <TableCell className='text-center font-bold p-4'>
+                    <Badge className='rounded-full'>
+                        Open
+                    </Badge>
+                </TableCell>
+                <TableCell className="text-right p-4">
+                    <span className='font-semibold'>
+                        $250.00
+                    </span>
+                </TableCell>
             </TableRow>
-    </TableHeader>
-    <TableBody>
-        <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-    </TableBody>
+        </TableBody>
     </Table>
 </div>
 
